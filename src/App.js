c82d603d6531f0468de6feeeb2f124bf0cb11778
郭建利 <guojianli@ihappy.net.cn>
2020-04-09 15:22:23 +0800

    声明周期函数

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LifeCircleComponent from './pages/LifeCircleComponent'
+
+function App() {
+  return (
+    <div>
+      <LifeCircleComponent />
+    </div>
+  )
+}
+
+export default App
