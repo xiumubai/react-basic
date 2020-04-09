e6090b6e171bc1c4775c1d5d090fc0b3c1b3db40
郭建利 <guojianli@ihappy.net.cn>
2020-04-09 21:46:36 +0800

    自定义hook

--- src/pages/HookPage.js
+++ src/pages/HookPage.js
@@ -2,11 +2,25 @@ import React, { useState, useEffect } from 'react'
 
 export default function HookPage(props) {
   const [count, setCount] = useState(0)
-  const [date, setDate] = useState(new Date())
+
   useEffect(() => {
     console.log('count effect')
   }, [count])
 
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>add</button>
+      <p>{useClock().toLocaleTimeString()}</p>
+    </div>
+  )
+}
+
+// 自定义一个hook，命名以use开头
+// 好处：组件之间可以重用逻辑，不增加额外的组件
+// 函数内部可以调用其他的hook
+function useClock() {
+  const [date, setDate] = useState(new Date())
   useEffect(() => {
     console.log('date effect')
     const timer = setInterval(() => {
@@ -14,11 +28,5 @@ export default function HookPage(props) {
     }, 1000)
     return () => clearInterval(timer)
   }, [])
-  return (
-    <div>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>add</button>
-      <p>{date.toLocaleTimeString()}</p>
-    </div>
-  )
+  return date
 }
